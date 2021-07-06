3d5afbc4fc191ce50ef46ffbb83bd62b86657dba
Christian P. Glahn <glah@zhaw.ch>
2021-07-06 09:29:11 +0200

    bootstrapping code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 // The core file for the moodle autopilot
 
-const moodle = require("./lib/moodle.js");
-const intention = require("./lib/intentionjs");
+const Moodle = require("../lib/moodle.js");
+// const intention = require("./lib/intention.js");
 
+
+Moodle.initMoodle("http://clean-vervet", "teacher2@lo-f.at", "9036f5b3e1c220a41e1690d3c8909c93");
