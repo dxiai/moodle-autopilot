0b56fa99353b9f3fc808b446628e4776d698b75c
Christian P. Glahn <glah@zhaw.ch>
2021-07-07 11:55:50 +0200

    new workflow format

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 // The core file for the moodle autopilot
 
-const Moodle = require("../lib/moodle.js");
+import {IntentionScript} from "../lib/intention.mjs";
 // const intention = require("./lib/intention.js");
 
+const MyToken = "9036f5b3e1c220a41e1690d3c8909c93";
+const filename = "./test-examples/assign_autograding.yaml";
 
-Moodle.initMoodle("http://clean-vervet", "teacher2@lo-f.at", "9036f5b3e1c220a41e1690d3c8909c93");
+async function main() {
+    const script = new IntentionScript(filename);
+
+    await script.load();
+    await script.parseAllIntentions();
+    await script.prepareConnection(MyToken);
+    await script.runIntentions();
+}
+
+main().then(() => {});
