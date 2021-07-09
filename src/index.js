e70f850a158c466df45a35b6dae9141fc4c94971
Christian P. Glahn <glah@zhaw.ch>
2021-07-09 22:26:56 +0200

    better error reporting and api changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // The core file for the moodle autopilot
-
+import { MoodleError } from "../lib/moodle.mjs";
+import { IntentionError } from "../lib/errors.mjs";
 import {IntentionScript} from "../lib/intention.mjs";
 // const intention = require("./lib/intention.js");
 
@@ -18,4 +19,22 @@ async function main(argv) {
     await script.runIntentions();
 }
 
-main(process.argv.slice(2)).then(() => {});
+main(process.argv.slice(2)).catch((err) => {
+    console.log("Script failed at");
+    console.log(`${err.message}`);
+
+    if (err instanceof MoodleError) {
+        console.log(err.function);
+        console.log(err.url);
+        if (err.data) {
+            console.log(err.data);
+        }
+        console.log(err.code);
+        console.log(err.info);
+    }
+    if (err instanceof IntentionError && err.intention) {
+        console.log(err.intention);
+    }
+    console.log("\n\n");
+    console.log(err);
+});
