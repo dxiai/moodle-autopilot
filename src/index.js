dbe8fc2643d1b751824b78a448a24524af7f4f0b
Christian P. Glahn <glah@zhaw.ch>
2021-07-09 09:37:28 +0200

    More secure calling structure.

--- src/index.js
+++ src/index.js
@@ -3,16 +3,19 @@
 import {IntentionScript} from "../lib/intention.mjs";
 // const intention = require("./lib/intention.js");
 
-const MyToken = "9036f5b3e1c220a41e1690d3c8909c93";
-const filename = "./test-examples/assign_autograding.yaml";
+// const MyToken = "9036f5b3e1c220a41e1690d3c8909c93";
+// const filename = "./test-examples/assign_autograding.yaml";
 
-async function main() {
+async function main(argv) {
+    const filename = shift argv;
+    const moodle_token = process.env.MOODLE_TOKEN;
+
     const script = new IntentionScript(filename);
 
     await script.load();
     await script.parseAllIntentions();
-    await script.prepareConnection(MyToken);
+    await script.prepareConnection(moodle_token);
     await script.runIntentions();
 }
 
-main().then(() => {});
+main(process.argv.slice(2))).then(() => {});
